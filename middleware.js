8bf4f8ecb78af68799fb7fea34c6099e41c8e2c1
Gabriel <gabrielrochasant@gmail.com>
2024-04-14 00:34:58 -0300

    feat: add search functionality to middleware

--- middleware.js
+++ middleware.js
@@ -3,30 +3,57 @@ const net = require("net");
 const services = {};
 
 const validate = (request) => {
-  return request.address && request.service_name;
+  switch (request.type) {
+    case "register":
+      return request.address && request.service_name;
+    case "search":
+      return request.service_name;
+    default:
+      return false;
+  }
 };
 
-const registerService = (request) => {
-  if (services[request.address]) {
-    console.log("Service already registered");
+const registerService = (socket, address, name) => {
+  services[name] = address;
+  socket.write(JSON.stringify({ status: "success" }));
+};
+
+const searchService = (socket, name) => {
+  const address = services[name];
+  if (address) {
+    socket.write(JSON.stringify({ status: "success", address: address }));
   } else {
-    services[request.address] = request.service_name;
+    socket.write(
+      JSON.stringify({ status: "error", message: "Service not found" })
+    );
   }
 };
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    console.log(Date.now(), data.toString());
+    console.log("recieved request", data.toString());
     const request = JSON.parse(data.toString());
     if (validate(request)) {
-      registerService(request);
-      socket.write(JSON.stringify({ status: "success" }));
+      switch (request.type) {
+        case "register":
+          registerService(socket, request.address, request.service_name);
+          break;
+        case "search":
+          searchService(socket, request.service_name);
+          break;
+        default:
+          socket.write(
+            JSON.stringify({ status: "error", message: "Invalid request type" })
+          );
+      }
     } else {
-      socket.write(JSON.stringify({ status: "error" }));
+      socket.write(
+        JSON.stringify({ status: "error", message: "Invalid request" })
+      );
     }
   });
 });
 
 server.listen(1337, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 1337");
 });
