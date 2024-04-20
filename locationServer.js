19aa51abd362d7322ffb9228e0ab4936a8f6f384
Raquel <raquel.lima.124@ufrn.edu.br>
2024-04-20 14:18:30 -0300

    refactor: Refactor localizacaoServer.js to locationServer.js and update variable and comment names to English

--- locationServer.js
+++ locationServer.js
@@ -1,6 +1,6 @@
 import net from "net";
 
-const localizacaoData = {
+const locationData = {
   A123: { latitude: 40.7128, longitude: -74.006 },
   B456: { latitude: 34.0522, longitude: -118.2437 },
   C789: { latitude: 51.5074, longitude: -0.1278 },
@@ -10,27 +10,27 @@ const port = 8080;
 
 const serverInfo = {
   type: "register",
-  service_name: "Servidor de informações de localizacao de transporte",
+  service_name: "Transport Location Information Server",
   address: `localhost:${port}`,
 };
 
 const client = new net.Socket();
 
 client.connect(1337, "localhost", () => {
-  console.log("Conectado ao middleware");
+  console.log("Connected to middleware");
   client.write(`${JSON.stringify(serverInfo)}`);
   client.on("data", (data) => {
-    console.log("Resposta do middleware:", data.toString().trim());
+    console.log("Middleware response:", data.toString().trim());
   });
   client.end();
 });
 
 client.on("close", () => {
-  console.log("Conexão com middleware fechada");
+  console.log("Connection with middleware closed");
 });
 
 const server = net.createServer((socket) => {
-  console.log("Cliente conectado");
+  console.log("Client connected");
 
   socket.on("data", (data) => {
     const requestData = data.toString().trim().split(":");
@@ -39,25 +39,25 @@ const server = net.createServer((socket) => {
 
     let response;
 
-     if (service === "localizacao") {
-      const location = localizacaoData[param];
+     if (service === "location") {
+      const location = locationData[param];
       if (location !== undefined) {
-        response = `A localização do ônibus ${param} é (${location.latitude}, ${location.longitude})`;
+        response = `The location of bus ${param} is (${location.latitude}, ${location.longitude})`;
       } else {
-        response = "Código de ônibus não encontrado";
+        response = "Bus code not found";
       }
     } else {
-      response = "Serviço não reconhecido";
+      response = "Service not recognized";
     }
 
     socket.write(response + "\r\n");
   });
 
   socket.on("end", () => {
-    console.log("Cliente desconectado");
+    console.log("Client disconnected");
   });
 });
 
 server.listen(port, "localhost", () => {
-  console.log(`Servidor TCP rodando em localhost:${port}`);
+  console.log(`TCP server running on localhost:${port}`);
 });
