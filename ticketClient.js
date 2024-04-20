07762a717147a25f8002000f0dbc242c22f2ac86
Raquel <raquel.lima.124@ufrn.edu.br>
2024-04-20 14:17:46 -0300

    refactor: Refactor passagemClient.js to ticketClient.js and update code to use Stub.getBusFare method

---
+++ ticketClient.js
@@ -0,0 +1,16 @@
+import Stub from "./stub.js";
+
+const questions = [
+    "ticket:54",
+    "ticket:44",
+    "ticket:46",
+    "ticket:73"
+]
+
+try {
+    for (const question of questions) {
+        console.log(await Stub.getBusFare("Transport Ticket Information Server",question))
+    }
+} catch (error) {
+    console.error(error);
+}
